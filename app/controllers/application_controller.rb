1d98e6f9e23f2aa6fd7bdc619fe89632f7343162
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-10-17 13:41:32 +0530

    Add user authentication and authorization features
    - Implement current_user and logged_in? helper methods.
    - Add require_user and require_admin methods for access control.
    - Create SessionsController for user login and logout functionality.
    - Add login form view for user authentication.
    - Create index and new views for displaying featured products and login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "You must be logged in to perform this action"
+      redirect_to login_path
+    end
+  end
+
+  def require_admin
+    unless logged_in? && current_user.role.name == 'admin'
+      flash[:alert] = "You must be an admin to perform this action"
+      redirect_to root_path
+    end
+  end
 end
